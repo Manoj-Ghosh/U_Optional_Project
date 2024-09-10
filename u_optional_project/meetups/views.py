3fd17d73279c7a95e9f8e1bc6d20ec0ecb66ca3d
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-09-10 12:18:33 +0530

    able to access the dabase stored data in the web page

--- u_optional_project/meetups/views.py
+++ u_optional_project/meetups/views.py
@@ -1,27 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Meetup 
 # Create your views here.
 
 
 def index(request):
     #return HttpResponse("Hello World !")
-    meetups = [
-        {
-            'title' : 'A First Meetups', 
-            'location': 'Durgapur', 
-            'slug': 'a-first-meetups'
-            
-         },
-
-        {
-            'title' : 'A Second Meetups',
-            'location': 'Kolkata', 
-            'slug': 'a-second-meetups'
-            
-        }
-
-    ]
+    meetups = Meetup.objects.all()
+    
     return render(request, 'meetups/index.html', {
         'show_meetups' : True,
         'meetups' : meetups
@@ -29,14 +16,15 @@ def index(request):
 
 
 def meetup_details(request, meetup_slug):
-    print(meetup_slug)
-    selected_meetups = {
 
-        'title':'A First Meetup',
-        'description':'This is the first meetup !'
-        
-        }
-    return render(request, 'meetups/meetups-details.html',{
-        'meetup_title': selected_meetups['title'],
-        'meetup_description':selected_meetups['description']
-    })
+    try:
+        selected_meetups = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetups-details.html', {
+            'meetup_found':True,
+            'meetup_title': selected_meetups.title,
+            'meetup_description':selected_meetups.description
+        })
+    except Exception as exc:
+        return render(request,'meetups/meetups-details.html',{
+            'meetup_found':False
+        } )
