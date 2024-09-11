d054a899b9b45efadffa1ae271fa73e8ccec85eb
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-09-11 13:20:17 +0530

    registration form is added in the meetup-details page

--- u_optional_project/meetups/views.py
+++ u_optional_project/meetups/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Meetup 
+from .forms import RegistrationForm
 # Create your views here.
 
 
@@ -19,9 +20,11 @@ def meetup_details(request, meetup_slug):
 
     try:
         selected_meetups = Meetup.objects.get(slug=meetup_slug)
+        registration_form = RegistrationForm()
         return render(request, 'meetups/meetups-details.html', {
             'meetup_found':True,
-            'meetup': selected_meetups
+            'meetup': selected_meetups,
+            'form': registration_form
         })
     except Exception as exc:
         return render(request,'meetups/meetups-details.html',{
