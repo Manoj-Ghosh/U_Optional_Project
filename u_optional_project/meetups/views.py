d6d450f33ef9784c18f6065e44286303b91c6637
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-09-11 13:41:51 +0530

    later i will give comment

--- u_optional_project/meetups/views.py
+++ u_optional_project/meetups/views.py
@@ -20,12 +20,26 @@ def meetup_details(request, meetup_slug):
 
     try:
         selected_meetups = Meetup.objects.get(slug=meetup_slug)
-        registration_form = RegistrationForm()
+        if request.method == 'GET':
+
+            
+            registration_form = RegistrationForm()
+       
+
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetups.Participants.add(participant)
+
         return render(request, 'meetups/meetups-details.html', {
+
             'meetup_found':True,
             'meetup': selected_meetups,
             'form': registration_form
         })
+
+
     except Exception as exc:
         return render(request,'meetups/meetups-details.html',{
             'meetup_found':False
